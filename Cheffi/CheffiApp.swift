3db422f3c19f2b6d3d66def5ef57dcad4dcad8ab
JEONGKUNHO <dghj6739@gmail.com>
2024-05-28 21:38:37 +0900

    Initial Commit

---
+++ Cheffi/CheffiApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheffiApp.swift
+//  Cheffi
+//
+//  Created by 정건호 on 5/28/24.
+//
+
+import SwiftUI
+
+@main
+struct CheffiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cheffi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cheffi
+//
+//  Created by 정건호 on 5/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

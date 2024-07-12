1e5edb3793ce7408d63248b2aa9a3b0d918c512e
textobey <dogsan_e@naver.com>
2024-07-13 00:46:03 +0900

    feature: LaunchScreen -> LoginView 화면 전환 구현, 화면 전환 기초 설계

--- Cheffi/Module/Feature/LaunchScreen/LaunchScreenView.swift
+++ Cheffi/Module/Feature/LaunchScreen/LaunchScreenView.swift
@@ -14,19 +14,23 @@ struct LaunchScreenView: View {
     
     var body: some View {
         WithPerceptionTracking {
-            ZStack {
-                Color.cheffiPrimary
-                    .ignoresSafeArea()
-                
-                Image("launchScreenLogo")
-                    .resizable()
-                    .frame(width: 120, height: 36)
+            NavigationStack(path: $store.scope(state: \.path, action: \.path)) {
+                ZStack {
+                    Color.cheffiPrimary
+                        .ignoresSafeArea()
+                    
+                    Image("launchScreenLogo")
+                        .resizable()
+                        .frame(width: 120, height: 36)
+                }
+            } destination: { store in
+                LoginView(store: store)
             }
             .onAppear {
                 store.send(.onAppear)
             }
             .alert(
-                $store.scope(state: \.destination?.alert, action: \.destination.alert)
+                $store.scope(state: \.alert, action: \.alert)
             )
         }
     }

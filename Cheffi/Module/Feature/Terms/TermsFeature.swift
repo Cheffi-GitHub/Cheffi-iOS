a9ab47c2a476e3d06a91ee3102d1febe2e740942
textobey <dogsan_e@naver.com>
2024-07-23 21:59:17 +0900

    feature: TermsView 추가

---
+++ Cheffi/Module/Feature/Terms/TermsFeature.swift
@@ -0,0 +1,29 @@
+//
+//  TermsFeature.swift
+//  Cheffi
+//
+//  Created by 이서준 on 7/23/24.
+//
+
+import ComposableArchitecture
+
+@Reducer
+struct TermsFeature {
+    
+    @ObservableState
+    struct State {
+        
+    }
+    
+    enum Action {
+        
+    }
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+                
+            }
+        }
+    }
+}

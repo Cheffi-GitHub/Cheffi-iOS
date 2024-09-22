9d0671d3dbc00f0ca8400ff1f138192a6120e16b
textobey <dogsan_e@naver.com>
2024-09-22 14:44:34 +0900

    feat: WebView 공통 NavigationBar 적용

--- Cheffi/Module/Feature/Web/WebView.swift
+++ Cheffi/Module/Feature/Web/WebView.swift
@@ -13,40 +13,24 @@ struct WebView: View {
     @Perception.Bindable var store: StoreOf<WebFeature>
     
     var body: some View {
-        ZStack {
-            Color.white
-                .edgesIgnoringSafeArea(.all)
-            
-            VStack(spacing: 0) {
-                ZStack {
-                    HStack {
-                        Button(action: {
-                            // TODO: 공통 Custom NavigationBar ViewModifier 생성
-                        }) {
-                            Image(name: Common.leftArrow)
-                                .resizable()
-                                .renderingMode(.template)
-                                .frame(width: 24, height: 24)
-                                .foregroundStyle(.black)
-                        }
-                        .buttonStyle(.plain)
-                        .padding(.leading, 16)
-                        
-                        Spacer()
-                    }
-                    
-                    Text(store.title)
-                        .font(.suit(.semiBold, 16))
-                        .foregroundStyle(.black)
-                }
-                .background(.white)
-                .frame(height: 44)
+        WithPerceptionTracking {
+            ZStack {
+                Color.white
+                    .edgesIgnoringSafeArea(.all)
                 
                 RepresentableWebView(url: store.url)
             }
+            .ignoresSafeArea(edges: .bottom)
+            .toolbar(.hidden)
+            .navigationBar(
+                title: "",
+                leftBarItems: [
+                    .backButton {
+                        store.send(.dismiss)
+                    }
+                ]
+            )
         }
-        .ignoresSafeArea(edges: .bottom)
-        .toolbar(.hidden)
     }
 }
 

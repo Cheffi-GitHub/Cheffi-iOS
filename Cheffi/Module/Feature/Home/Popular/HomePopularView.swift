8a085403a28aa87ce85074403917a3ca933f8c7c
ericKwon95 <ericyong95@gmail.com>
2024-09-24 05:25:45 +0900

    refactor: HomePopularView 작은 단위로 뷰 분리

--- Cheffi/Module/Feature/Home/Popular/HomePopularView.swift
+++ Cheffi/Module/Feature/Home/Popular/HomePopularView.swift
@@ -27,151 +27,18 @@ struct HomePopularView: View {
         WithPerceptionTracking {
             NavigationStack(path: $store.scope(state: \.path, action: \.path)) {
                 VStack(alignment: .leading, spacing: 0) {
-                    Text("인기 급등 맛집")
-                        .foregroundStyle(.black)
-                        .font(.suit(.bold, 20))
-                        .padding(.bottom, 16)
-                        .padding(.leading, 16)
+                    headline
                     if store.popularReviews.count == 0 {
-                        VStack(alignment: .center, spacing: 0) {
-                            Image(name: Home.homeEmpty)
-                                .padding(.bottom, 12)
-                            Text("아직 주변의 맛집 리뷰가 없어요\n먼저 주변 아는 맛집을 소개해주세요!")
-                                .font(.suit(.medium, 14))
-                                .foregroundStyle(Color.grey6)
-                                .padding(.bottom, 18)
-                                .multilineTextAlignment(.center)
-                            Text("맛집 직접 등록하기")
-                                .font(.suit(.semiBold, 15))
-                                .foregroundStyle(Color.primary)
-                                .padding(.horizontal, 14)
-                                .padding(.vertical, 9)
-                                .background(Color.background)
-                                .clipShape(.rect(cornerRadius: 10))
-                        }
+                        reviewEmpty
+                            .padding(.top, 40)
                     } else {
-                        VStack(alignment: .leading, spacing: 0) {
-                            HStack(spacing: 0) {
-                                Image(name: Common.clock)
-                                Text(store.remainTime.toHourMinuteSecond())
-                                    .foregroundStyle(Color.primary)
-                                    .font(.suit(.bold, 18))
-                                Text("초 뒤에")
-                                    .foregroundStyle(.black)
-                                    .font(.suit(.bold, 18))
-                            }
-                            HStack(spacing: 4) {
-                                Text("인기 급등 맛집이 변경돼요.")
-                                    .foregroundStyle(Color.black)
-                                    .font(.suit(.regular, 18))
-                                Image(name: Common.info)
-                                    .overlay(
-                                        Group {
-                                            if store.state.showTooltip {
-                                                Image(name: Home.popularTooptip)
-                                                    .offset(x: -60, y: 50)
-                                            }
-                                        }
-                                    )
-                                    .onTapGesture {
-                                        store.send(.toolTipTapped)
-                                    }
-                                Spacer()
-                                NavigationLink(state: HomePopularFeature.Path.State.moveToAllReviewView(
-                                    .init(popularReviews: store.popularReviews, remainTime: store.remainTime)
-                                )) {
-                                    HStack {
-                                        Text("전체보기")
-                                            .foregroundStyle(Color.grey6)
-                                            .font(.suit(.medium, 14))
-                                        Image(name: Common.rightArrow)
-                                            .resizable()
-                                            .renderingMode(.template)
-                                            .frame(width: 16, height: 16)
-                                            .foregroundStyle(Color.grey6)
-                                    }
-                                }
-                            }
-                        }
-                        .zIndex(1)
-                        .padding(.horizontal, 16)
-                        .padding(.bottom, 24)
-                        
-                        TabView(selection: $currentpage) {
-                            ForEach(0..<store.totalPage, id: \.self) { index in
-                                WithPerceptionTracking {
-                                    VStack {
-                                        if index == 0 {
-                                            NavigationLink(
-                                                state: HomePopularFeature.Path.State.moveToReviewDetailView(.init(id: store.popularReviews[0].id))
-                                            ) {
-                                                ReviewCell(review: store.popularReviews[0], type: .medium)
-                                            }
-                                            LazyVGrid(columns: columns) {
-                                                if store.popularReviews.count-1 >= 1 {
-                                                    NavigationLink(
-                                                        state: HomePopularFeature.Path.State.moveToReviewDetailView(.init(id: store.popularReviews[1].id))
-                                                    ) {
-                                                        ReviewCell(review: store.popularReviews[1], type: .small)
-                                                    }
-                                                }
-                                                if store.popularReviews.count-1 >= 2 {
-                                                    NavigationLink(
-                                                        state: HomePopularFeature.Path.State.moveToReviewDetailView(.init(id: store.popularReviews[2].id))
-                                                    ) {
-                                                        ReviewCell(review: store.popularReviews[2], type: .small)
-                                                    }
-                                                }
-                                            }
-                                        } else {
-                                            LazyVGrid(columns: columns) {
-                                                ForEach(0..<4) { offset in
-                                                    let reviewIndex = 3 + (index - 1) * 4 + offset
-                                                    if store.popularReviews.count-1 >= reviewIndex {
-                                                        NavigationLink(
-                                                            state: HomePopularFeature.Path.State.moveToReviewDetailView(.init(id: store.popularReviews[reviewIndex].id))
-                                                        ) {
-                                                            ReviewCell(review: store.popularReviews[reviewIndex], type: .small)
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                    .padding(.horizontal, 16)
-                                    .tag(index + 1)
-                                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
-                                }
-                            }
-                        }
-                        .frame(height: store.popularReviews.count == 1 ? 270 : 573, alignment: .center)
-                        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-                        .animation(.easeInOut, value: currentpage)
-                        // 탭뷰 페이징
-                        HStack(spacing: 0) {
-                            Spacer()
-                            Image(name: Home.previousPage)
-                                .padding(.trailing, 12)
-                                .onTapGesture {
-                                    if currentpage != 1 {
-                                        currentpage -= 1
-                                    }
-                                }
-                            Text("\(currentpage)")
-                                .foregroundStyle(Color.black)
-                                .font(.suit(.medium, 16))
-                            Text(" / \(store.state.totalPage)")
-                                .foregroundStyle(Color.grey8)
-                                .font(.suit(.medium, 16))
-                            Image(name: Home.nextPage)
-                                .padding(.leading, 12)
-                                .onTapGesture {
-                                    if currentpage != store.state.totalPage {
-                                        currentpage += 1
-                                    }
-                                }
-                            Spacer()
-                        }
+                        timeIndicator
+                            .zIndex(1)
+                            .padding(.horizontal, 16)
+                            .padding(.bottom, 24)
+                            .padding(.top, 16)
+                        tabView
+                        paging
                     }
                 }
                 .onChange(of: scenePhase) { state in
@@ -197,6 +64,167 @@ struct HomePopularView: View {
                     }
                 }
             }
+        }
+    }
+    
+    private var headline: some View {
+        Text("인기 급등 맛집")
+            .foregroundStyle(.black)
+            .font(.suit(.bold, 20))
+                    .padding(.leading, 16)
+    }
+    
+    private var reviewEmpty: some View {
+        VStack(alignment: .center, spacing: 0) {
+            Image(name: Home.homeEmpty)
+                .padding(.bottom, 12)
+            // TODO: linehgight 수정 필요
+            Text("아직 주변의 맛집 리뷰가 없어요\n먼저 주변 아는 맛집을 소개해주세요!")
+                .font(.suit(.medium, 14))
+                .foregroundStyle(Color.grey6)
+                .padding(.bottom, 18)
+                .multilineTextAlignment(.center)
+            Text("맛집 직접 등록하기")
+                .font(.suit(.semiBold, 15))
+                .foregroundStyle(Color.primary)
+                .padding(.horizontal, 14)
+                .padding(.vertical, 9)
+                .background(Color.background)
+                .clipShape(.rect(cornerRadius: 10))
+        }
+    }
+    
+    private var timeIndicator: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            HStack(spacing: 0) {
+                Image(name: Common.clock)
+                Text(store.remainTime.toHourMinuteSecond())
+                    .foregroundStyle(Color.primary)
+                    .font(.suit(.bold, 18))
+                Text("초 뒤에")
+                    .foregroundStyle(.black)
+                    .font(.suit(.bold, 18))
+            }
+            HStack(spacing: 4) {
+                Text("인기 급등 맛집이 변경돼요.")
+                    .foregroundStyle(Color.black)
+                    .font(.suit(.regular, 18))
+                Image(name: Common.info)
+                    .overlay(
+                        Group {
+                            if store.state.showTooltip {
+                                Image(name: Home.popularTooptip)
+                                    .offset(x: -60, y: 50)
+                            }
+                        }
+                    )
+                    .onTapGesture {
+                        // TODO: 다른 영역 탭 시에도 툴팁 사라지는 기능 구현 필요
+                        store.send(.toolTipTapped)
+                    }
+                Spacer()
+                NavigationLink(state: HomePopularFeature.Path.State.moveToAllReviewView(
+                    .init(popularReviews: store.popularReviews, remainTime: store.remainTime)
+                )) {
+                    HStack {
+                        // TODO: 전체보기 탭 했을 때 리뷰 전체보기 화면으로 네비게이션 기능 구현 필요
+                        Text("전체보기")
+                            .foregroundStyle(Color.grey6)
+                            .font(.suit(.medium, 14))
+                        Image(name: Common.rightArrow)
+                            .resizable()
+                            .renderingMode(.template)
+                            .frame(width: 16, height: 16)
+                            .foregroundStyle(Color.grey6)
+                    }
+                }
+            }
+        }
+    }
+    
+    private var tabView: some View {
+        TabView(selection: $currentpage) {
+            ForEach(0..<store.totalPage, id: \.self) { index in
+                WithPerceptionTracking {
+                    VStack {
+                        // 첫 페이지
+                        if index == 0 {
+                            NavigationLink(
+                                state: HomePopularFeature.Path.State.moveToReviewDetailView(.init(id: store.popularReviews[0].id))
+                            ) {
+                                ReviewCell(review: store.popularReviews[0], type: .medium)
+                            }
+                            // TODO: 일반 Grid로 변경 필요
+                            LazyVGrid(columns: columns) {
+                                if store.popularReviews.count-1 >= 1 {
+                                    NavigationLink(
+                                        state: HomePopularFeature.Path.State.moveToReviewDetailView(.init(id: store.popularReviews[1].id))
+                                    ) {
+                                        ReviewCell(review: store.popularReviews[1], type: .small)
+                                    }
+                                }
+                                if store.popularReviews.count-1 >= 2 {
+                                    NavigationLink(
+                                        state: HomePopularFeature.Path.State.moveToReviewDetailView(.init(id: store.popularReviews[2].id))
+                                    ) {
+                                        ReviewCell(review: store.popularReviews[2], type: .small)
+                                    }
+                                }
+                            }
+                        } else {
+                            // 이후 페이지
+                            LazyVGrid(columns: columns) {
+                                ForEach(0..<4) { offset in
+                                    WithPerceptionTracking {
+                                        let reviewIndex = 3 + (index - 1) * 4 + offset
+                                        if store.popularReviews.count-1 >= reviewIndex {
+                                            NavigationLink(
+                                                state: HomePopularFeature.Path.State.moveToReviewDetailView(.init(id: store.popularReviews[reviewIndex].id))
+                                            ) {
+                                                ReviewCell(review: store.popularReviews[reviewIndex], type: .small)
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    .padding(.horizontal, 16)
+                    .tag(index + 1)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+                }
+            }
+        }
+        .frame(height: store.popularReviews.count == 1 ? 270 : 573, alignment: .center)
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+        .animation(.easeInOut, value: currentpage)
+    }
+    
+    private var paging: some View {
+        // 탭뷰 페이징
+        HStack(spacing: 0) {
+            Spacer()
+            Image(name: Home.previousPage)
+                .padding(.trailing, 12)
+                .onTapGesture {
+                    if currentpage != 1 {
+                        currentpage -= 1
+                    }
+                }
+            Text("\(currentpage)")
+                .foregroundStyle(Color.black)
+                .font(.suit(.medium, 16))
+            Text(" / \(store.state.totalPage)")
+                .foregroundStyle(Color.grey8)
+                .font(.suit(.medium, 16))
+            Image(name: Home.nextPage)
+                .padding(.leading, 12)
+                .onTapGesture {
+                    if currentpage != store.state.totalPage {
+                        currentpage += 1
+                    }
+                }
+            Spacer()
         }
     }
 }

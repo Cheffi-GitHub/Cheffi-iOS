8b18460b16ed9db3dce67c2b230e1b3b1482a32c
ericKwon95 <ericyong95@gmail.com>
2024-09-26 06:00:15 +0900

    feat: dummy ReviewDetailModel 작성

--- Cheffi/Module/Component/Models/ReviewDetailModel.swift
+++ Cheffi/Module/Component/Models/ReviewDetailModel.swift
@@ -41,6 +41,64 @@ struct ReviewDetailModel: Codable, Equatable {
         case photos
         case menus
     }
+    
+    static let dummy: ReviewDetailModel = .init(
+    id: 1,
+    title: "태초동에 생긴 맛집!",
+    text: "초밥 태초세트 추천해요",
+    bookmarked: false,
+    ratedByUser: false,
+    ratingType: "GOOD",
+    createdDate: "2024-09-25T18:57:17.865Z",
+    timeLeftToLock: 86399751,
+    matchedTagNum: 3,
+    restaurant: Restaurant(
+        id: 3,
+        name: "을밀대",
+        address: Address(
+            province: "서울특별시",
+            city: "양천구",
+            roadName: "숭문길 24",
+            fullLotNumberAddress: "서울시 마포구 염리동 111",
+            fullRoadNameAddress: "서울시 마포구 숭문길 24"
+        ),
+        registered: true
+    ),
+    writer: Writer(
+        id: 1,
+        nickname: "닉네임1234",
+        photo: PhotoInfo(
+            url: "https://www~~~.com",
+            width: 320,
+            height: 320
+        ),
+        introduction: "안녕하세요, 리뷰 작성자입니다.",
+        writtenByViewer: false
+    ),
+    ratings: Ratings(
+        good: 0,
+        average: 0,
+        bad: 0
+    ),
+    photos: [
+        Photo(
+            id: 1,
+            order: 1,
+            photo: PhotoInfo(
+                url: "https://www~~~.com",
+                width: 320,
+                height: 320
+            )
+            )
+    ],
+    menus: [
+        Menu(
+            name: "곱창 전골",
+            price: 80000,
+            description: "string"
+        )
+    ]
+    )
 }
 
 

16c04356234892d6e132b0c73507743a17894b27
textobey <dogsan_e@naver.com>
2024-10-14 16:50:21 +0900

    feat: 가입 환영 화면, 이동 시나리오 구현

---
+++ Cheffi/Module/Feature/Welcome/WelcomeFeature.swift
@@ -0,0 +1,8 @@
+//
+//  WelcomeFeature.swift
+//  Cheffi
+//
+//  Created by 이서준 on 10/2/24.
+//
+
+import Foundation
---
+++ Cheffi/Module/Feature/Welcome/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  Cheffi
+//
+//  Created by 이서준 on 10/2/24.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
